842e16fd031c45afbeed2d1c99c594cc58505b3c
fedabeqain <feda.beqain@gmail.com>
2023-09-25 01:33:25 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply ; 
+let num1 = 2 ;
+let num2 = 31 ; 
+multiply = num1*num2; 
 
-// Write your code here
+let random  ; 
+random  = Math.floor(Math.random() * 10) +  1;
+
+let mod ; 
+let num3 = 18;
+let num4= 7 ;
+mod = num3 % num4 ;
+
+let max ; 
+ max = Math.max( 1, 2, 4 ,20)
